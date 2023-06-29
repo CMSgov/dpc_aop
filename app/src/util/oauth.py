ea732427f3b56c33876cf4f8d9896a232555fbd7
Christopher Ronning <cronning90@gmail.com>
2023-06-29 14:52:01 -0400

    api example case

--- app/src/util/oauth.py
+++ app/src/util/oauth.py
@@ -1,17 +1,20 @@
 import os
 import requests
+from src.util.local import load_local_env_vars
 
 def get_pecos_access_token() -> dict[str, str]:
+    load_local_env_vars()
     url = os.getenv('PECOS_API_TOKEN_URL', None)
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'cache-control': 'no-cache'
     }
     params = {
-        'grant_type': os.getenv('PECOS_GRANT_TYPE', None),
-        'client_id': os.gentenv('PECOS_CLIENT_ID', None),
-        'client_secret': os.getnev('PECOS_CLIENT_SECRET', None),
-        'scope': os.getenv('PECOS_CLIENT_SCOPE', None)
+        'grant_type': 'client_credentials',
+        'client_id': os.getenv('PECOS_CLIENT_ID', None),
+        'client_secret': os.getenv('PECOS_CLIENT_SECRET', None),
+        'scope': 'READ'
     }
     response = requests.post(url, headers=headers, params=params)
-    return response.json()
+    json = response.json()
+    return json['access_token']
